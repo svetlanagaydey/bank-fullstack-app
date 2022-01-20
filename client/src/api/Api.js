84a33d30899323bb3809110e83fbdda23fb8a7b9
svetlanagaydey <lana84@ukr.net>
2022-01-21 01:51:59 +0200

    still fixing

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 let myUrl = 'http://localhost:8080/api/'; //development
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.PORT === 'production') {
   myUrl = '/api';
 }
 export default axios.create({
