db5be4de873f7b1ad2cbbcf14be4ef707c810988
svetlanagaydey <lana84@ukr.net>
2022-01-22 18:46:23 +0200

    client page designe completed

--- client/src/Components/Pages/UserPage/UserPage.jsx
+++ client/src/Components/Pages/UserPage/UserPage.jsx
@@ -7,37 +7,51 @@ import myApi from '../../../api/Api';
 const UserPage = () => {
     const [searchingId, setSearchingId] = useState("");
     const [currentUser, setCarrentUser] = useState();
+    const inputRef = React.createRef();
 
     const findUser = async() => {
         console.log("yes");
-        const { data } = await myApi.get(`/users/${searchingId}`);
-        setCarrentUser(data);
-        console.log(data)
+        try {
+            const { data } = await myApi.get(`/users/${searchingId}`);
+            setCarrentUser(data);
+            console.log(data)
+        } catch (e) {
+            console.log(e);
+        }
+        
     }
     useEffect(() => {
+        inputRef.current.focus();
+    },[currentUser]);
 
-    },[currentUser])
     const printUserInfo = () => {
         return (
+          <div className="userOptions">
             <div className="client">
-                <span className="client-icon"></span>
-                <div className="info-block">
-                    <p className="firstName"><span className="formSubtitles">First Name: </span>{currentUser.user.firstName}</p>
-                    <p className="lastName"><span className="formSubtitles">Last Name: </span>{currentUser.user.lastName}</p>
-                    <p className="id"><span className="formSubtitles">id: </span>{currentUser.user._id}</p>
-                    <p className="credit"><span className="formSubtitles">credit: </span>{currentUser.user.cash}</p>
-                    <p className="cash"><span className="formSubtitles">cash: </span>{currentUser.user.credit}</p>
-                </div>
-			</div>
+              <span className="client-icon"></span>
+              <div className="info-block">
+                <p className="firstName"><span className="formSubtitles">First Name: </span>{currentUser.user.firstName}</p>
+                <p className="lastName"><span className="formSubtitles">Last Name: </span>{currentUser.user.lastName}</p>
+                <p className="id"><span className="formSubtitles">id: </span>{currentUser.user._id}</p>
+                <p className="credit"><span className="formSubtitles">credit: </span>{currentUser.user.cash}</p>
+                <p className="cash"><span className="formSubtitles">cash: </span>{currentUser.user.credit}</p>
+              </div>
+            </div>
+            <ul className="options-list">
+              <li className="userOption deposit"> <span className="icon depositIcon"></span>Deposit</li>
+              <li className="userOption withdraw"> <span className="icon withdrawIcon"></span>Withdraw</li>
+              <li className="userOption updateName"> <span className="icon updateNameIcon"></span>Update</li>
+              <li className="userOption delete"> <span className="icon deleteIcon"></span>Delete</li>
+            </ul>
+          </div>
         )
     }
     return (
         <div className="container userPage">
             <Header />
-            <h2 className="user-header">Client Operations</h2>
-            <form className="search-id-bloc">
-                <label>Enter Client's Id for search: </label>
-                <input type="text" name="searchingId" value={searchingId}
+            <h2 className="user-header">Enter Client's ID to searching</h2>
+            <form className="searching-bloc">
+                <input type="text" name="searchingId" value={searchingId} ref={inputRef} 
                     onChange={(e) => {setSearchingId(e.target.value)}}
                  />
                 <button className="searchButton" type="button" onClick={() => findUser()}>Find</button>
