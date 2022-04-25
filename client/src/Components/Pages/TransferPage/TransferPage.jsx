0063bac98198c563b3d4b68b00032602f9b26aa6
Svetlana Gaydey <lana84@ukr.net>
2022-04-25 22:41:15 +0300

    Transfer request wordes

--- client/src/Components/Pages/TransferPage/TransferPage.jsx
+++ client/src/Components/Pages/TransferPage/TransferPage.jsx
@@ -4,6 +4,7 @@ import TransferClient from './TransferClient';
 import { useState, useContext } from "react";
 import './transferPage.css';
 import { Context } from '../../../Context';
+import myApi from '../../../api/Api';
 
 
 const TransferPage = () => {
@@ -14,10 +15,32 @@ const TransferPage = () => {
     setAmount(e.target.value);
   }
 
-  const transfer = (e) => {
+  const transfer = async(e) => {
     e.preventDefault();
     console.log(amount);
     console.log(context);
+    if (context.from.cash+context.from.credit < amount) {
+      console.log("Not enouth money");
+      return
+    }
+    try {
+			await myApi.put(`/users/withdraw/${context.from._id}`, {
+				amount: amount
+			})
+			console.log("withdrawed")
+		} catch (err) {
+			console.log(err.message);
+		}
+
+    try {
+			await myApi.put(`/users/deposit/${context.to._id}`, {
+				amount: parseFloat(amount)
+			})
+			console.log("deposited")
+			
+		} catch (err) {
+			console.log(err.message);
+		}
   }
 
   return (
