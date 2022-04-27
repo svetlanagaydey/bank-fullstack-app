5c972b65eeaf63522a88f826a83c5284795e17df
Svetlana Gaydey <lana84@ukr.net>
2022-04-27 08:18:04 +0300

    Cleint fields to transfer component

--- client/src/Components/Pages/TransferPage/TransferClient.jsx
+++ client/src/Components/Pages/TransferPage/TransferClient.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import myApi from '../../../api/Api';
 import './transferPage.css';
 import { Context } from "../../../Context";
@@ -16,13 +16,18 @@ const TransferClient = (props) => {
 		setPassport(e.target.value)	
 	}
 
+	useEffect(() => {
+console.log(client);
+console.log(isUser)
+	}, [isUser])
+
 	const onHandleSubmit = async(e) => {
 		e.preventDefault();
 		try {
 			console.log(passport)
 			const data = await myApi.get(`/users/transfer/${passport}`);
-			// console.log(data.data)
 			setClient(data.data);
+			setIsUser(true);
 			console.log(e.target.name)
 			if (e.target.name === "transfer-from") {
 				setContext({...context, from: data.data});
@@ -30,7 +35,6 @@ const TransferClient = (props) => {
 			if (e.target.name === "transfer-to") {
 				setContext({...context, to: data.data})
 			}
-			setIsUser(true);
 		} catch (e) {
 			setIsUser(false);
 			setClient("");
@@ -44,6 +48,22 @@ const TransferClient = (props) => {
 				<input type="text" onChange={(e) => onHandleChange(e)} />
 				<button type='submit'>Find</button>
 			</form>
+			{isUser && (
+				<div className="transfer__client-data">
+					<div className="transfer__client-field">
+						<span className="transfer__field-name">Name: </span>
+						<span className="transfer__field-value">{client.firstName} {client.lastName}</span>
+					</div>
+					<div className="transfer__client-field">
+						<span className="transfer__field-name">Cash: </span>
+						<span className="transfer__field-value">{client.cash}</span>
+					</div>
+					<div className="transfer__client-field">
+						<span className="transfer__field-name">Credit: </span>
+						<span className="transfer__field-value">{client.credit}</span>
+					</div>
+				</div>
+			)}
 		</div>
 	)
 }
