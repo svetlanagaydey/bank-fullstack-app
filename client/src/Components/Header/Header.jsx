3b17d03ebeadc8f92e547a4b5a473fb377982ddc
Svetlana Gaydey <lana84@ukr.net>
2022-05-04 13:11:09 +0300

    Start page responsive to mobile

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './header.css';
 import atom from '../../Assets/Images/atom.png';
+import atomMobile from '../../Assets/Images/atom-mobile.png';
 const Header = () => {
     return (
             <Link className="header" to='/'>
                 <div className="header-logo">
+                <picture>
+                    <source srcset={atomMobile} media="(min-width: 1000px)" />
                     <img src={atom} alt=""/>
+                </picture>    
                 </div>
                 <h2>To Main Page</h2>
             </Link>
