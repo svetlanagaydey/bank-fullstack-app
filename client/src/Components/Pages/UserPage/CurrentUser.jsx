a2826e0129abb5c056f02647dba78b849c59c98d
Svetlana Gaydey <lana84@ukr.net>
2022-05-08 16:48:01 +0300

    Client page mobile styles

--- client/src/Components/Pages/UserPage/CurrentUser.jsx
+++ client/src/Components/Pages/UserPage/CurrentUser.jsx
@@ -7,7 +7,7 @@ const CurrentUser = ({currentUser}) => {
     <span className="client-icon"></span>
     <div className="client-block">
       <p className="current-passport"><span className="formSubtitles">Passport: </span>{currentUser.passport}</p>
-      <p className="current-name"><span className="formSubtitles">Name: </span>{currentUser.firstName} {currentUser.lastName}</p>
+      <p className="current-name"><span className="formSubtitles">Full Name: </span>{currentUser.firstName} {currentUser.lastName}</p>
       <p className="current-credit"><span className="formSubtitles">Credit: </span>{currentUser.credit}</p>
       <p className="current-cash"><span className="formSubtitles">Cash: </span>{currentUser.cash}</p>
       <p className="current-birthDay"><span className="formSubtitles">Birth: </span>{currentUser.birthDay}</p>
